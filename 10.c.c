1bc5f6bca02bf7da20eb9541a934bbeae0ebf5ba
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2022-12-28 23:30:37 +0530

    Add files via upload

---
+++ 10.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+//Programing language.c
+{    
+    char x='S';
+    char y='R';
+    char z='B';
+    printf("%c\n%c\n%c\n",x,y,z);
+
+}
---
+++ 9.c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the values a");
+    scanf("%d",&a);
+    printf("enter the values b");
+    scanf("%d",&b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+}
