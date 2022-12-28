e1c8f7740bd9594073ab3aac7582d5e28a7e3807
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2022-12-28 23:44:11 +0530

    Add files via upload

---
+++ C-Tutorials/8.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,sum;
+    printf("enter the values a,b,c\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("sum is: %d",a+b+c);
+}
