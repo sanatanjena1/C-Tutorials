b2194346345455c6888b30f6ec601a74e2d4b01f
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2022-12-30 23:25:42 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+//area of square
+{
+    int side;
+    printf("enter side values");
+    scanf("%d",&side);
+    printf("area is: %d",side*side);
+    return 0;
+}
---
+++ 14.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+//area of circle
+{
+     float radius;
+     printf("enter radius values");
+     scanf("%f",& radius);
+     printf("area is: %f",3.14*radius*radius);
+}
